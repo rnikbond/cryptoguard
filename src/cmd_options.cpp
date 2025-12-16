7a2ef4005ee4288f15121d4a436d8b2d88928abe
rnikbond <rnikbond@gmail.com>
2025-12-16 12:52:58 +0300

    Добавлена обработка аргументов командной строки

--- src/cmd_options.cpp
+++ src/cmd_options.cpp
@@ -1,11 +1,77 @@
 #include "cmd_options.h"
+#include <boost/program_options/parsers.hpp>
+#include <boost/program_options/variables_map.hpp>
+#include <filesystem>
+#include <iostream>
+#include <ranges>
+#include <stdexcept>
+
+namespace fs = std::filesystem;
+namespace po = boost::program_options;
 
 namespace CryptoGuard {
 
-ProgramOptions::ProgramOptions() : desc_("Allowed options") {}
+ProgramOptions::ProgramOptions() : desc_("Allowed options") {
+
+    // Собираем доступные команды
+    std::string allowedCommands;
+    for (const auto &cmd : commandMapping_ | std::views::keys) {
+        if (!allowedCommands.empty()) {
+            allowedCommands += ", ";
+        }
+        allowedCommands += cmd;
+    }
+
+    desc_.add_options()("help,h", "Available commands");
+    desc_.add_options()("command,c",
+                        po::value<std::string>()
+                            ->notifier([this](std::string value) {
+                                auto it = commandMapping_.find(value);
+                                if (it != commandMapping_.end()) {
+                                    command_ = it->second;
+                                } else {
+                                    throw std::runtime_error("unknown command");
+                                }
+                            })
+                            ->required(),
+                        allowedCommands.data());
+    desc_.add_options()("input,i",
+                        po::value<std::string>()
+                            ->notifier([this](std::string path) {
+                                if (fs::is_regular_file(path)) {
+                                    this->inputFile_ = path;
+                                } else {
+                                    throw std::runtime_error("input file does not exists");
+                                }
+                            })
+                            ->required(),
+                        "input file data");
+    desc_.add_options()("output,o", po::value<std::string>(&outputFile_)->default_value("result.out"),
+                        "output file for result");
+    desc_.add_options()("password,p", po::value<std::string>(&password_)->composing(),
+                        "password for encrypt and decrypt");
+}
 
 ProgramOptions::~ProgramOptions() = default;
 
-void ProgramOptions::Parse(int argc, char *argv[]) {}
+void ProgramOptions::Parse(int argc, char *argv[]) {
+
+    try {
+        po::variables_map argsMap;
+        po::parsed_options argsParsed = po::parse_command_line(argc, argv, desc_);
+        po::store(argsParsed, argsMap);
+
+        if (argsMap.count("help")) {
+            std::cout << "--- HELP MESSAGE ---" << std::endl;
+            std::cout << desc_ << std::endl;
+            exit(0);
+        }
+
+        po::notify(argsMap);
+
+    } catch (...) {
+        throw;
+    }
+}
 
 }  // namespace CryptoGuard
--- src/main.cpp
+++ src/main.cpp
@@ -38,69 +38,56 @@ int main(int argc, char *argv[]) {
         //
         // OpenSSL пример использования:
         //
-        std::string input = "01234567890123456789";
-        std::string output;
+        // std::string input = "01234567890123456789";
+        // std::string output;
 
-        OpenSSL_add_all_algorithms();
+        // OpenSSL_add_all_algorithms();
 
-        auto params = CreateChiperParamsFromPassword("12341234");
-        params.encrypt = 1;
-        auto *ctx = EVP_CIPHER_CTX_new();
+        // auto params = CreateChiperParamsFromPassword("12341234");
+        // params.encrypt = 1;
+        // auto *ctx = EVP_CIPHER_CTX_new();
 
-        // Инициализируем cipher
-        EVP_CipherInit_ex(ctx, params.cipher, nullptr, params.key.data(), params.iv.data(), params.encrypt);
+        // // Инициализируем cipher
+        // EVP_CipherInit_ex(ctx, params.cipher, nullptr, params.key.data(), params.iv.data(), params.encrypt);
 
-        std::vector<unsigned char> outBuf(16 + EVP_MAX_BLOCK_LENGTH);
-        std::vector<unsigned char> inBuf(16);
-        int outLen;
+        // std::vector<unsigned char> outBuf(16 + EVP_MAX_BLOCK_LENGTH);
+        // std::vector<unsigned char> inBuf(16);
+        // int outLen;
 
-        // Обрабатываем первые N символов
-        std::copy(input.begin(), std::next(input.begin(), 16), inBuf.begin());
-        EVP_CipherUpdate(ctx, outBuf.data(), &outLen, inBuf.data(), static_cast<int>(16));
-        for (int i = 0; i < outLen; ++i) {
-            output.push_back(outBuf[i]);
-        }
+        // // Обрабатываем первые N символов
+        // std::copy(input.begin(), std::next(input.begin(), 16), inBuf.begin());
+        // EVP_CipherUpdate(ctx, outBuf.data(), &outLen, inBuf.data(), static_cast<int>(16));
+        // for (int i = 0; i < outLen; ++i) {
+        //     output.push_back(outBuf[i]);
+        // }
 
-        // Обрабатываем оставшиеся символы
-        std::copy(std::next(input.begin(), 16), input.end(), inBuf.begin());
-        EVP_CipherUpdate(ctx, outBuf.data(), &outLen, inBuf.data(), static_cast<int>(input.size() - 16));
-        for (int i = 0; i < outLen; ++i) {
-            output.push_back(outBuf[i]);
-        }
+        // // Обрабатываем оставшиеся символы
+        // std::copy(std::next(input.begin(), 16), input.end(), inBuf.begin());
+        // EVP_CipherUpdate(ctx, outBuf.data(), &outLen, inBuf.data(), static_cast<int>(input.size() - 16));
+        // for (int i = 0; i < outLen; ++i) {
+        //     output.push_back(outBuf[i]);
+        // }
 
-        // Заканчиваем работу с cipher
-        EVP_CipherFinal_ex(ctx, outBuf.data(), &outLen);
-        for (int i = 0; i < outLen; ++i) {
-            output.push_back(outBuf[i]);
-        }
-        EVP_CIPHER_CTX_free(ctx);
-        std::print("String encoded successfully. Result: '{}'\n\n", output);
-        EVP_cleanup();
+        // // Заканчиваем работу с cipher
+        // EVP_CipherFinal_ex(ctx, outBuf.data(), &outLen);
+        // for (int i = 0; i < outLen; ++i) {
+        //     output.push_back(outBuf[i]);
+        // }
+        // EVP_CIPHER_CTX_free(ctx);
+        // std::print("String encoded successfully. Result: '{}'\n\n", output);
+        // EVP_cleanup();
         //
         // Конец примера
         //
 
         CryptoGuard::ProgramOptions options;
+        options.Parse(argc, argv);
+        CryptoGuard::ProgramOptions::COMMAND_TYPE cmd = options.GetCommand();
+        std::print("cmd: {}\n", static_cast<int>(cmd));
 
-        CryptoGuard::CryptoGuardCtx cryptoCtx;
+        // CryptoGuard::CryptoGuardCtx cryptoCtx;
 
-        using COMMAND_TYPE = CryptoGuard::ProgramOptions::COMMAND_TYPE;
-        switch (options.GetCommand()) {
-        case COMMAND_TYPE::ENCRYPT:
-            std::print("File encoded successfully\n");
-            break;
-
-        case COMMAND_TYPE::DECRYPT:
-            std::print("File decoded successfully\n");
-            break;
-
-        case COMMAND_TYPE::CHECKSUM:
-            std::print("Checksum: {}\n", "CHECKSUM_NOT_IMPLEMENTED");
-            break;
-
-        default:
-            throw std::runtime_error{"Unsupported command"};
-        }
+        // using COMMAND_TYPE = CryptoGuard::ProgramOptions::COMMAND_TYPE;
 
     } catch (const std::exception &e) {
         std::print(std::cerr, "Error: {}\n", e.what());
